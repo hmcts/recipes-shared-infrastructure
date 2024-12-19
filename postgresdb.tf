188ec02b0ed1690be185d85b116f4d06697fa406
Marty Fox <martin.fox1@justice.gov.uk>
2024-12-19 11:47:43 +0000

    references in vault mistake

--- postgresdb.tf
+++ postgresdb.tf
@@ -30,29 +30,35 @@ module "postgresql_flexible" {
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   name         = "recipe-backend-POSTGRES-USER"
   value        = module.postgresql_flexible.username
-  key_vault_id = module.vault.key_vault_name.key_vault_id
+  key_vault_id = module.vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name         = "recipe-backend-POSTGRES-PASS"
   value        = module.postgresql_flexible.password
-  key_vault_id = module.vault.key_vault_name.key_vault_id
+  key_vault_id = module.vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   name         = "recipe-backend-POSTGRES-HOST"
   value        = module.postgresql_flexible.fqdn
-  key_vault_id = module.vault.key_vault_name.key_vault_id
+  key_vault_id = module.vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
   name         = "recipe-backend-POSTGRES-PORT"
   value        = "5432"
-  key_vault_id = module.vault.key_vault_name.key_vault_id
+  key_vault_id = module.vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name         = "recipe-backend-POSTGRES-DATABASE"
-  value        = "rhubarb"
-  key_vault_id = module.vault.key_vault_name.key_vault_id
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE_CFT" {
+  name         = "recipe-backend-POSTGRES-DATABASE_CFT"
+  value        = "plum"
+  key_vault_id = module.vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE_SDS" {
+  name         = "recipe-backend-POSTGRES-DATABASE_SDS"
+  value        = "toffee"
+  key_vault_id = module.vault.key_vault_id
 }
