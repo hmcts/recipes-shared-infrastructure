5c967a4f0a064c53e103640749f6b298cd27fbf3
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-04-27 14:28:49 +0100

    add jenkins mi access

--- keyvault.tf
+++ keyvault.tf
@@ -7,12 +7,18 @@ module "vault" {
   env                                  = var.env
   tenant_id                            = var.tenant_id
   object_id                            = var.jenkins_AAD_objectId
+  jenkins_object_id                    = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name                  = azurerm_resource_group.shared_resource_group.name
   product_group_object_id              = "e7ea2042-4ced-45dd-8ae3-e051c6551789"
   common_tags                          = local.tags
   managed_identity_object_id           = var.managed_identity_object_id
   create_managed_identity              = true
   additional_managed_identities_access = var.additional_managed_identities_access
+}
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
 }
 
 output "vaultName" {
